bbac7a2df3cf0cbea0b60b9fc20403bb9965afb9
Egemen Göl <egemengol@gmail.com>
2022-01-23 14:00:50 +0300

    Add Netlify adapter

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,18 +1,31 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
+export default {
 	preprocess: preprocess(),
-
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter({
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		}),
 		target: '#svelte'
 	}
 };
 
-export default config;
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://github.com/sveltejs/svelte-preprocess
+// 	// for more information about preprocessors
+// 	preprocess: preprocess(),
+
+// 	kit: {
+// 		adapter: adapter(),
+
+// 		// hydrate the <div id="svelte"> element in src/app.html
+// 		target: '#svelte'
+// 	}
+// };
+
+// export default config;
